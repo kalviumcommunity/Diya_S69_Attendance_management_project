7b937e119b62fc91ba4064559bf2afdc6a6e7fe9
DIYA <diya94816@gmail.com>
2025-09-09 09:00:06 +0530

    committed

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -2,24 +2,24 @@ package com.school;
 
 public class Course {
     private int courseId;
-    private String title;
+    private String courseName;
     private int credits;
     private static int nextCourseIdCounter = 101;
 
-    // Constructor with title and credits (ID auto-generated)
-    public Course(String title, int credits) {
+    // Constructor with course name and credits (ID auto-generated)
+    public Course(String courseName, int credits) {
         this.courseId = nextCourseIdCounter++;
-        this.title = title;
+        this.courseName = courseName;
         this.credits = credits;
     }
 
     // Getters
     public int getCourseId() { return courseId; }
-    public String getTitle() { return title; }
+    public String getCourseName() { return courseName; }
     public int getCredits() { return credits; }
 
     // Method to display course details
     public void displayInfo() {
-        System.out.println("Course ID: C" + courseId + ", Title: " + title + ", Credits: " + credits);
+        System.out.println("Course ID: C" + courseId + ", Course Name: " + courseName + ", Credits: " + credits);
     }
 }
